bfa7d868384d5eebb078dd639c59f933c3506802
mandysulli <xpa3@cdc.gov>
2025-02-21 15:54:04 -0500

    final solution for getting gc content

--- gc/src/main.rs
+++ gc/src/main.rs
@@ -4,6 +4,7 @@ use std::mem::replace;
 
 fn main() {
     //read in fasta file
+    // command needs to be: cargo run /filepath/test.txt
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
     let contents = fs::read_to_string(file_path)
@@ -66,4 +67,27 @@ fn main() {
     }
     //check that the gc values are storing correctly
     //println!("{:?}", gc);
+
+    //Inialize elemnets to hold the highest gc content and the header associated with highest gc content
+    let mut hold_gc: f32 = 0.0;
+    let mut hold_name = &String::new();
+    let mut index = 0;
+    //Interate through the gc contents stored in gc vector
+    //when the a value is higher than the gc value being held in hold gc, store new value in hold_gc 
+    //and store associated header in hold name
+    for i in gc {
+        if i > hold_gc {
+            hold_gc = i.clone();
+            hold_name = header.get(index).unwrap();
+        } else {
+            continue
+        }
+        index+=1;
+    }
+
+    //Convert outputs to specified format and print
+    let final_name = hold_name.replace(">", "");
+    let final_gc = hold_gc*100.0;
+    println!("{}", final_name);
+    println!("{}", final_gc);
 }
