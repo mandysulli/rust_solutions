e238a8e92948c63e66ae4dbb73eab07b8f95c612
Samuel Shepard <vfn4@cdc.gov>
2025-02-27 13:25:15 -0500

    Auto-format

--- src/bin/gc.rs
+++ src/bin/gc.rs
@@ -7,8 +7,7 @@ fn main() {
     // command needs to be: cargo run /filepath/test.txt
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
-    let contents = fs::read_to_string(file_path)
-    .expect("Should have been able to read the file");
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     //Initialize vectors to store headers and sequences
     let mut header: Vec<String> = Vec::new();
@@ -23,25 +22,23 @@ fn main() {
         //If line starts with '>' save to header vec and the increase counter
         if line.starts_with('>') {
             header.push(line.to_string());
-            header_counter+=1;
+            header_counter += 1;
         //If line  does not start with '>' move into seq handling
         } else {
             //If the sequence counter is less than the header counter then push first sequence to the seq vector
             //then increase the seq counter
             if seq_counter < header_counter {
                 seq.push(line.to_string());
-                seq_counter+=1;
+                seq_counter += 1;
                 hold_seq = line.to_string().clone();
             //If the sequence counter is equal to the header counter then amend the current sequence onto the seq saved in vector
             //this takes the seqeunces from different lines and smushes them into one full seq
-            } else if seq_counter == header_counter  {
-                let position: usize = (seq_counter-1).try_into().unwrap();
+            } else if seq_counter == header_counter {
+                let position: usize = (seq_counter - 1).try_into().unwrap();
                 hold_seq.push_str(line);
                 //It doesn't liek the way I use this but it lets it slide...
                 std::mem::replace(&mut seq[position], hold_seq.clone());
-
             }
-            
         }
     }
     //check that sequence conversion works
@@ -53,17 +50,16 @@ fn main() {
     //Then divide that by the length of the sequence
     for i_seq in seq {
         let mut gc_counter = 0;
-        let seq_length=i_seq.len();
+        let seq_length = i_seq.len();
         for j in i_seq.chars() {
-            if j == 'G'{
+            if j == 'G' {
                 gc_counter += 1;
             } else if j == 'C' {
                 gc_counter += 1;
             }
         }
-        let mut gc_content: f32= gc_counter as f32/seq_length as f32;
+        let mut gc_content: f32 = gc_counter as f32 / seq_length as f32;
         gc.push(gc_content)
-        
     }
     //check that the gc values are storing correctly
     //println!("{:?}", gc);
@@ -73,21 +69,21 @@ fn main() {
     let mut hold_name = &String::new();
     let mut index = 0;
     //Interate through the gc contents stored in gc vector
-    //when the a value is higher than the gc value being held in hold gc, store new value in hold_gc 
+    //when the a value is higher than the gc value being held in hold gc, store new value in hold_gc
     //and store associated header in hold name
     for i in gc {
         if i > hold_gc {
             hold_gc = i.clone();
             hold_name = header.get(index).unwrap();
         } else {
-            continue
+            continue;
         }
-        index+=1;
+        index += 1;
     }
 
     //Convert outputs to specified format and print
     let final_name = hold_name.replace(">", "");
-    let final_gc = hold_gc*100.0;
+    let final_gc = hold_gc * 100.0;
     println!("{}", final_name);
     println!("{}", final_gc);
 }
