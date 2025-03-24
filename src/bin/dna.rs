eca2d3f4fa7cfa673f7aabf0a8a32b38631ec99d
mandysulli <xpa3@cdc.gov>
2025-03-24 10:26:17 -0400

    complete with dna challenge

--- src/bin/dna.rs
+++ src/bin/dna.rs
@@ -1,15 +1,22 @@
+use std::env;
+use std::fs;
 use std::io;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
     let mut my_string = String::new();
 
     // TODO: IF an argument is provided, read from that, otherwise read from the
     // stdin like below.
-    println!("Enter your DNA sequence below:");
-    io::stdin()
-        .read_line(&mut my_string)
-        .expect("Failed to read line");
-
+    if args.len() > 1 {
+        let file_path = &args[1];
+        my_string = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    } else {
+        println!("Enter your DNA sequence below:");
+        io::stdin()
+            .read_line(&mut my_string)
+            .expect("Failed to read line");
+    }
     my_string.make_ascii_uppercase();
 
     let mut a_count = 0;
